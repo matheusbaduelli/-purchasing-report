19544a071195b1e4a5c7426bd637e77e24a2d200
Matheus Rodrigues de Lima <mhs.djc@gmail.com>
2025-02-23 20:59:53 -0300

    MultSomaSe

--- teste.py
+++ teste.py
@@ -21,14 +21,17 @@ lista_pedidos.to_sql('lista_de_pedidos',conn,if_exists='replace',index=False)
 
 
 
-cursor.execute("SELECT lista_de_pedidos.Pedido,lista_tecnica.CódigodoProduto,lista_tecnica.DescriçãodoProduto,lista_tecnica.CódigoN1,lista_tecnica.DescricaoN1,lista_tecnica.Qtd,lista_tecnica.CódigoN2,lista_tecnica.DescricaoN2,lista_tecnica.Qtd1,lista_tecnica.CódigoN3,lista_tecnica.DescricaoN3,lista_tecnica.Qtd2,lista_tecnica.CódigoN4,lista_tecnica.DescricaoN4,lista_tecnica.Qt3,lista_tecnica.Somase,lista_tecnica.Somase1,lista_tecnica.Somase2 FROM lista_de_pedidos INNER JOIN lista_tecnica ON lista_de_pedidos.Codigo = lista_tecnica.CódigodoProduto;")
+cursor.execute("SELECT lista_de_pedidos.Pedido,lista_tecnica.CódigodoProduto,lista_tecnica.DescriçãodoProduto,lista_tecnica.CódigoN1,lista_tecnica.DescricaoN1,lista_tecnica.Qtd,lista_tecnica.CódigoN2,lista_tecnica.DescricaoN2,lista_tecnica.Qtd1,lista_tecnica.CódigoN3,lista_tecnica.DescricaoN3,lista_tecnica.Qtd2,lista_tecnica.CódigoN4,lista_tecnica.DescricaoN4,lista_tecnica.Qt3,lista_tecnica.Somase,lista_tecnica.Somase1,lista_tecnica.Somase2,lista_de_pedidos.Qtd FROM lista_de_pedidos INNER JOIN lista_tecnica ON lista_de_pedidos.Codigo = lista_tecnica.CódigodoProduto;")
 
 
 
 nova_lista = []
 # Iterar sobre os resultados
 for linha in cursor.fetchall():
-    lista = {"Pedido":linha[0],"Produto":linha[1],"Descricao":linha[2],"CódigoN1":linha[3],"DescricaoN1":linha[4],"Qtd":linha[5],"CódigoN2":linha[6],"DescricaoN2":linha[7],"Qtd1":linha[8],"CódigoN3":linha[9],"DescricaoN3":linha[10],"Qtd2":linha[11],"CódigoN4":linha[12],"DescricaoN4":linha[13],"Qt3":linha[14],"SomaSe":linha[15],"SomaSe1":linha[16],"SomaSe2":linha[17]}
+    lista = {"Pedido":linha[0],"Produto":linha[1],"Descricao":linha[2],"CódigoN1":linha[3],"DescricaoN1":linha[4],"Qtd":linha[5],"CódigoN2":linha[6],"DescricaoN2":linha[7],"Qtd1":linha[8],"CódigoN3":linha[9],"DescricaoN3":linha[10],"Qtd2":linha[11],"CódigoN4":linha[12],"DescricaoN4":linha[13],"Qt3":linha[14],"SomaSe":linha[15],"SomaSe1":linha[16],"SomaSe2":linha[17],"QtdPedido":linha[18],"qtd": (linha[15] or 0) * (linha[18] or 0),
+    "QtdMultiSomase1": (linha[16] or 0) * (linha[18] or 0),
+    "QtdMultiSomase2": (linha[17] or 0) * (linha[18] or 0)
+}
     nova_lista.append(lista)
 # Criar DataFrame com os resultados
 df =  pd.DataFrame(nova_lista)
